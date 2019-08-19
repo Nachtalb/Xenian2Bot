b9591a4a3f9b73a6fcc06f4a216c53d0a2c8573e
Nick Espig <nickespig@gmail.com>
2019-08-19 22:34:19 +0200

    Improve backend

--- bot/admin.py
+++ bot/admin.py
@@ -1,18 +1,53 @@
 from django.contrib import admin
 
-from bot.models.groupsettings import GroupSettings
+from bot.models.groupsettings import GroupSettings, Warnings
 from bot.models.usersettings import UserSettings
 
 
+@admin.register(UserSettings)
 class UserSettingsAdmin(admin.ModelAdmin):
-    list_display = ['user_id', 'username', 'user_fullname', 'modified', 'created']
+    fieldsets = (
+        ('Info', {
+            'fields': ('user_id', 'username', 'user_fullname')
+        }),
+    )
+
+    readonly_fields = ('user_id', 'username', 'user_fullname',)
+    list_display = ('user_id', 'username', 'user_fullname', 'modified', 'created')
+    search_fields = ('user_id', 'username', 'user_fullname',)
 
 
-admin.site.register(UserSettings, UserSettingsAdmin)
-
-
+@admin.register(GroupSettings)
 class GroupSettingsAdmin(admin.ModelAdmin):
-    list_display = ['group_id', 'grouptitle', 'groupname', 'modified', 'created']
+    fieldsets = (
+        ('Info', {
+            'fields': ('group_id', 'grouptitle', 'groupname')
+        }),
+        ('Settings', {
+            'fields': ('rules', 'welcome',),
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('dev_mode',),
+        }),
+    )
+
+    list_display = ('group_id', 'grouptitle', 'groupname', 'has_rules', 'has_welcome', 'dev_mode', 'modified', 'created')
+    readonly_fields = ('group_id', 'grouptitle', 'groupname')
+    search_fields = ('group_id', 'grouptitle', 'groupname', 'rules', 'welcome')
+
+    def has_rules(self, obj):
+        return bool(obj.rules)
+
+    has_rules.boolean = True
+
+    def has_welcome(self, obj):
+        return bool(obj.welcome)
+
+    has_welcome.boolean = True
 
 
-admin.site.register(GroupSettings, GroupSettingsAdmin)
+@admin.register(Warnings)
+class WarningsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'group', 'count', 'modified', 'created')
+    autocomplete_fields = ('user', 'group')
