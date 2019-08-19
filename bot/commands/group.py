ff5ea1c90b327b16f17a0785126a5a473465ce87
Nick Espig <nickespig@gmail.com>
2019-08-19 20:37:36 +0200

    Let user define kick duration

--- bot/commands/group.py
+++ bot/commands/group.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+import pytimeparse
 from telegram.ext import Filters
 
 from bot.commands import BaseCommand
@@ -37,14 +38,14 @@ class Group(BaseCommand):
             return
         self.message.reply_text(f'Banned {user.link or user.user_fullname}')
 
-    def _kick(self, user: UserSettings):
+    def _kick(self, user: UserSettings, delta: timedelta = None):
         if check_permissions(self.chat, user.user, 'can_restrict_members'):
             return False
 
         if self.group_settings.dev_mode:
             return
 
-        until = datetime.now() + timedelta(minutes=1)
+        until = datetime.now() + (delta if delta else timedelta(minutes=1))
         self.chat.kick_member(user.user_id, until_date=until)
 
     @BaseCommand.command_wrapper(filters=Filters.group & OwnFilters.check_permission('can_restrict_members'))
@@ -54,10 +55,22 @@ class Group(BaseCommand):
             return
         user = self.get_user_settings(self.message.reply_to_message.from_user)
 
-        if self._kick(user) is False:
+        time_text = self.message.text.replace('/kick', '').strip()
+        delta = None
+        if time_text:
+            delta = pytimeparse.parse(time_text)
+            if delta is None:
+                self.message.reply_text('Could not understand time. Use something like 10m or 1h etc.')
+                return
+            if not 30 < delta < 31622400:
+                self.message.reply_text('Duration below 30 sec or above 366 days result in a ban. For that use /ban.')
+                return
+            delta = timedelta(seconds=delta)
+
+        if self._kick(user, delta) is False:
             self.message.reply_text(f'Could not kick {user.link or user.user_fullname}')
             return
-        self.message.reply_text(f'Kicked {user.link or user.user_fullname}')
+        self.chat.send_message(f'Kicked {user.link or user.user_fullname}', reply_to_message_id=self.message.reply_to_message.message_id)
 
     @BaseCommand.command_wrapper(filters=Filters.group)
     def rules(self):
